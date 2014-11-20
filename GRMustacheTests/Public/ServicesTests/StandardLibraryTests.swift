63a40444a0c9cc3eb27d9a89f166420ffdaee3a0
Gwendal Roué <gr@pierlis.com>
2014-11-20 12:39:10 +0100

    StandardLibraryTests WIP

--- GRMustacheTests/Public/ServicesTests/StandardLibraryTests.swift
+++ GRMustacheTests/Public/ServicesTests/StandardLibraryTests.swift
@@ -52,4 +52,22 @@ class StandardLibraryTests: XCTestCase {
         rendering = template.render(Value(["object": renderable]))!
         XCTAssertEqual(rendering, "&lt;br&gt;")
     }
+    
+    func testStandardLibraryJavascriptEscapeDoesEscapeRenderable() {
+        let renderable = Value({ (tag: Tag, renderingInfo: RenderingInfo, contentType: ContentTypePointer, error: NSErrorPointer) -> (String?) in
+            return "\"double quotes\" and 'single quotes'"
+        })
+        let template = Template(string: "{{# javascript.escape }}{{ object }}{{/ }}")!
+        let rendering = template.render(Value(["object": renderable]))!
+        XCTAssertEqual(rendering, "\\u0022double quotes\\u0022 and \\u0027single quotes\\u0027")
+    }
+    
+    func testStandardLibraryURLEscapeDoesEscapeRenderingObjects() {
+        let renderable = Value({ (tag: Tag, renderingInfo: RenderingInfo, contentType: ContentTypePointer, error: NSErrorPointer) -> (String?) in
+            return "&"
+        })
+        let template = Template(string: "{{# URL.escape }}{{ object }}{{/ }}")!
+        let rendering = template.render(Value(["object": renderable]))!
+        XCTAssertEqual(rendering, "%26")
+    }
 }
