fdd3156e660b8ae2dd3c5d2451a2ea28bdb80839
Gwendal Roué <gr@pierlis.com>
2014-11-21 09:00:19 +0100

    TagTests WIP

--- GRMustacheTests/Public/TagTests.swift
+++ GRMustacheTests/Public/TagTests.swift
@@ -44,4 +44,38 @@ class TagTests: XCTestCase {
         range = tagObserver.tagDescription?.rangeOfString("{{  name\t}}")
         XCTAssertTrue(range != nil)
     }
+
+    func testTagDescriptionContainsLineNumber() {
+        class TagDescriptionObserver: MustacheTagObserver {
+            var tagDescription: String?
+            init() {
+                tagDescription = nil
+            }
+            func mustacheTag(tag: Tag, willRenderValue value: Value) -> Value {
+                tagDescription = tag.description
+                return value
+            }
+            func mustacheTag(tag: Tag, didRender rendering: String?, forValue value: Value) {
+            }
+        }
+        let tagObserver = TagDescriptionObserver()
+        
+        var template = Template(string: "{{name}}")!
+        template.baseContext = template.baseContext.contextByAddingTagObserver(tagObserver)
+        template.render(Value())
+        var range = tagObserver.tagDescription?.rangeOfString("line 1")
+        XCTAssertTrue(range != nil)
+        
+        template = Template(string: "\n {{\nname}}")!
+        template.baseContext = template.baseContext.contextByAddingTagObserver(tagObserver)
+        template.render(Value())
+        range = tagObserver.tagDescription?.rangeOfString("line 2")
+        XCTAssertTrue(range != nil)
+        
+        template = Template(string: "\n\n  {{#\nname}}\n\n{{/name}}")!
+        template.baseContext = template.baseContext.contextByAddingTagObserver(tagObserver)
+        template.render(Value())
+        range = tagObserver.tagDescription?.rangeOfString("line 3")
+        XCTAssertTrue(range != nil)
+    }
 }
