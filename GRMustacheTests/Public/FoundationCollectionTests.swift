c8c2aa1406c6c58e92990f165dc55a262a8b59dd
Gwendal Roué <gr@pierlis.com>
2015-01-02 19:07:25 +0100

    WIP

--- GRMustacheTests/Public/FoundationCollectionTests.swift
+++ GRMustacheTests/Public/FoundationCollectionTests.swift
@@ -11,26 +11,26 @@ import GRMustache;
 
 class FoundationCollectionTests: XCTestCase {
     
-    var arrayValue: Box!
-    var setValue: Box!
-    var orderedSetValue: Box!
+    var boxedArray: Box!
+    var boxedSet: Box!
+    var boxedOrderedSet: Box!
     
     override func setUp() {
-        arrayValue = {
+        boxedArray = {
             var array = NSMutableArray()
             array.addObject(["key": "value"])
             var data = NSMutableDictionary()
             data.setObject(array, forKey: "collection")
             return Box(data)
         }()
-        setValue = {
+        boxedSet = {
             var set = NSMutableSet()
             set.addObject(["key": "value"])
             var data = NSMutableDictionary()
             data.setObject(set, forKey: "collection")
             return Box(data)
             }()
-        orderedSetValue = {
+        boxedOrderedSet = {
             var orderedSet = NSMutableOrderedSet()
             orderedSet.addObject(["key": "value"])
             var data = NSMutableDictionary()
@@ -40,72 +40,72 @@ class FoundationCollectionTests: XCTestCase {
     }
     
     func testNSArrayIsIterated() {
-        let rendering = Template(string: "{{#collection}}{{key}}{{/collection}}")!.render(arrayValue)!
+        let rendering = Template(string: "{{#collection}}{{key}}{{/collection}}")!.render(boxedArray)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSArrayIsNotIteratedWithNSArrayValueForKey() {
-        let rendering = Template(string: "{{#collection.key}}{{.}}{{/collection.key}}")!.render(arrayValue)!
+        let rendering = Template(string: "{{#collection.key}}{{.}}{{/collection.key}}")!.render(boxedArray)!
         XCTAssertEqual(rendering, "")
     }
     
     func testNSArrayCount() {
-        let rendering = Template(string: "{{collection.count}}")!.render(arrayValue)!
+        let rendering = Template(string: "{{collection.count}}")!.render(boxedArray)!
         XCTAssertEqual(rendering, "1")
     }
     
     func testNSArrayFirstObject() {
-        let rendering = Template(string: "{{collection.firstObject.key}}")!.render(arrayValue)!
+        let rendering = Template(string: "{{collection.firstObject.key}}")!.render(boxedArray)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSArrayLastObject() {
-        let rendering = Template(string: "{{collection.lastObject.key}}")!.render(arrayValue)!
+        let rendering = Template(string: "{{collection.lastObject.key}}")!.render(boxedArray)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSSetIsIterated() {
-        let rendering = Template(string: "{{#collection}}{{key}}{{/collection}}")!.render(setValue)!
+        let rendering = Template(string: "{{#collection}}{{key}}{{/collection}}")!.render(boxedSet)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSSetIsNotIteratedWithNSArrayValueForKey() {
-        let rendering = Template(string: "{{#collection.key}}{{.}}{{/collection.key}}")!.render(setValue)!
+        let rendering = Template(string: "{{#collection.key}}{{.}}{{/collection.key}}")!.render(boxedSet)!
         XCTAssertEqual(rendering, "")
     }
     
     func testNSSetCount() {
-        let rendering = Template(string: "{{collection.count}}")!.render(setValue)!
+        let rendering = Template(string: "{{collection.count}}")!.render(boxedSet)!
         XCTAssertEqual(rendering, "1")
     }
     
     func testNSSetAnyObject() {
-        let rendering = Template(string: "{{collection.anyObject.key}}")!.render(setValue)!
+        let rendering = Template(string: "{{collection.anyObject.key}}")!.render(boxedSet)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSOrderedSetIsIterated() {
-        let rendering = Template(string: "{{#collection}}{{key}}{{/collection}}")!.render(orderedSetValue)!
+        let rendering = Template(string: "{{#collection}}{{key}}{{/collection}}")!.render(boxedOrderedSet)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSOrderedSetIsNotIteratedWithNSArrayValueForKey() {
-        let rendering = Template(string: "{{#collection.key}}{{.}}{{/collection.key}}")!.render(orderedSetValue)!
+        let rendering = Template(string: "{{#collection.key}}{{.}}{{/collection.key}}")!.render(boxedOrderedSet)!
         XCTAssertEqual(rendering, "")
     }
     
     func testNSOrderedSetCount() {
-        let rendering = Template(string: "{{collection.count}}")!.render(orderedSetValue)!
+        let rendering = Template(string: "{{collection.count}}")!.render(boxedOrderedSet)!
         XCTAssertEqual(rendering, "1")
     }
     
     func testNSOrderedSetFirstObject() {
-        let rendering = Template(string: "{{collection.firstObject.key}}")!.render(orderedSetValue)!
+        let rendering = Template(string: "{{collection.firstObject.key}}")!.render(boxedOrderedSet)!
         XCTAssertEqual(rendering, "value")
     }
     
     func testNSOrderedSetLastObject() {
-        let rendering = Template(string: "{{collection.lastObject.key}}")!.render(orderedSetValue)!
+        let rendering = Template(string: "{{collection.lastObject.key}}")!.render(boxedOrderedSet)!
         XCTAssertEqual(rendering, "value")
     }
     
