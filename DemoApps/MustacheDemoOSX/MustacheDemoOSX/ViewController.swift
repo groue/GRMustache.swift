0625e36734c7ced41ba9bf7f1877693b04538858
Gwendal Roué <gr@pierlis.com>
2015-02-11 22:20:04 +0100

    Demo MustacheDemoOSX application WIP

--- DemoApps/MustacheDemoOSX/MustacheDemoOSX/ViewController.swift
+++ DemoApps/MustacheDemoOSX/MustacheDemoOSX/ViewController.swift
@@ -10,11 +10,9 @@ import Cocoa
 import Mustache
 
 class ViewController: NSViewController {
-    
     @IBOutlet var templateTextView: NSTextView!
     @IBOutlet var JSONTextView: NSTextView!
     @IBOutlet var renderingTextView: NSTextView!
-    
     @IBOutlet var model: Model!
     
     override func viewDidLoad() {
@@ -26,6 +24,20 @@ class ViewController: NSViewController {
     @IBAction func render(sender: AnyObject) {
         var error: NSError?
         if let template = Template(string: model.templateString, error: &error) {
+
+            // Uncomment and play with those goodies in your templates:
+            // They are documented at https://github.com/groue/GRMustache.swift/blob/master/Guides/goodies.md
+            
+//            let percentFormatter = NSNumberFormatter()
+//            percentFormatter.numberStyle = .PercentStyle
+//            template.registerInBaseContext("percent", Box(percentFormatter))
+//            template.registerInBaseContext("each", Box(StandardLibrary.each))
+//            template.registerInBaseContext("zip", Box(StandardLibrary.zip))
+//            template.registerInBaseContext("localize", Box(StandardLibrary.Localizer(bundle: nil, table: nil)))
+//            template.registerInBaseContext("HTMLEscape", Box(StandardLibrary.HTMLEscape))
+//            template.registerInBaseContext("URLEscape", Box(StandardLibrary.URLEscape))
+//            template.registerInBaseContext("javascriptEscape", Box(StandardLibrary.javascriptEscape))
+
             let data = model.JSONString.dataUsingEncoding(NSUTF8StringEncoding)!
             if let JSONObject: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &error) {
                 if let rendering = template.render(BoxAnyObject(JSONObject), error: &error) {
