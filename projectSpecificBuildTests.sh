6989abe01b80c8793cd016afaa60a7bff5a9cc1e
Vadim Eisenberg <vadime@il.ibm.com>
2016-02-14 15:36:44 +0200

    replaced Sources directory with Public in projectSpecificBuildTests.sh

--- projectSpecificBuildTests.sh
+++ projectSpecificBuildTests.sh
@@ -6,13 +6,13 @@
 #  Created by Vadim Eisenberg on 07/02/2016.
 #  Copyright © 2016 IBM. All rights reserved.
 
-cp Tests/Sources/*/*.mustache .build/debug/
-cp Tests/Sources/*/*.text .build/debug/
+cp Tests/Public/*/*.mustache .build/debug/
+cp Tests/Public/*/*.text .build/debug/
 cp -r Tests/vendor/groue/GRMustacheSpec/Tests .build/debug/
-cp -r Tests/Sources/TwitterHoganJSTests/HoganSuite .build/debug/
-cp -r Tests/Sources/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests .build/debug/
-cp -r Tests/Sources/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests_partial .build/debug/
-cp -r Tests/Sources/TemplateRepositoryBundleTests/TemplateRepositoryBundleTestsResources .build/debug/
-cp -r Tests/Sources/ServicesTests/LocalizerTestsBundle .build/debug/
-cp -r Tests/Sources/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests .build/debug/
-cp -r Tests/Sources/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests_* .build/debug/
+cp -r Tests/Public/TwitterHoganJSTests/HoganSuite .build/debug/
+cp -r Tests/Public/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests .build/debug/
+cp -r Tests/Public/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests_partial .build/debug/
+cp -r Tests/Public/TemplateRepositoryBundleTests/TemplateRepositoryBundleTestsResources .build/debug/
+cp -r Tests/Public/ServicesTests/LocalizerTestsBundle .build/debug/
+cp -r Tests/Public/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests .build/debug/
+cp -r Tests/Public/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests_* .build/debug/
