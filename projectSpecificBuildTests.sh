f4687d38ec10d0ceacbdb557eca4d89410daf178
Vadim Eisenberg <vadime@il.ibm.com>
2016-02-15 14:19:05 +0200

    fixed copied resurces in projectSpecificBuildTests.sh

--- projectSpecificBuildTests.sh
+++ projectSpecificBuildTests.sh
@@ -7,12 +7,13 @@
 #  Copyright © 2016 IBM. All rights reserved.
 
 cp Tests/Public/*/*.mustache .build/debug/
-cp Tests/Public/*/*.text .build/debug/
+cp Tests/Public/*/*/*.mustache .build/debug/
+cp Tests/Public/*/*/*.text .build/debug/
 cp -r Tests/vendor/groue/GRMustacheSpec/Tests .build/debug/
-cp -r Tests/Public/TwitterHoganJSTests/HoganSuite .build/debug/
-cp -r Tests/Public/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests .build/debug/
-cp -r Tests/Public/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests_partial .build/debug/
-cp -r Tests/Public/TemplateRepositoryBundleTests/TemplateRepositoryBundleTestsResources .build/debug/
+cp -r Tests/Public/SuitesTests/twitter/hogan.js/HoganSuite .build/debug/
+cp -r Tests/Public/TemplateRepositoryTests/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests .build/debug/
+cp -r Tests/Public/TemplateRepositoryTests/TemplateRepositoryBundleTests/TemplateRepositoryBundleTests_partial .build/debug/
+cp -r Tests/Public/TemplateRepositoryTests/TemplateRepositoryBundleTests/TemplateRepositoryBundleTestsResources .build/debug/
 cp -r Tests/Public/ServicesTests/LocalizerTestsBundle .build/debug/
-cp -r Tests/Public/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests .build/debug/
-cp -r Tests/Public/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests_* .build/debug/
+cp -r Tests/Public/TemplateRepositoryTests/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests .build/debug/
+cp -r Tests/Public/TemplateRepositoryTests/TemplateRepositoryFileSystemTests/TemplateRepositoryFileSystemTests_* .build/debug/
