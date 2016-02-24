ea0d77d1ec8f6e3b94b9973a69ccf0a3a12b8693
Vadim Eisenberg <vadime@il.ibm.com>
2016-02-24 11:55:55 +0200

    IBM-Swift/Kitura#84 added dependency on Bridging

--- Package.swift
+++ Package.swift
@@ -5,4 +5,8 @@ let package = Package(
   testDependencies: [
     .Package(url: "git@github.com:IBM-Swift/Kitura-TestFramework.git", versions: Version(0,2,0)..<Version(0,3,0))
   ]
-) 
+)
+
+#if !os(Linux)
+    package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Bridging.git", versions: Version(0,2,0)..<Version(0,3,0)))
+#endif
