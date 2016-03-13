a9784f4b6781a6abc3958327ddbe70288e3bccfa
Vadim Eisenberg <vadime@il.ibm.com>
2016-03-13 10:44:06 +0200

    IBM-Swift/Kitura#169 added dependency on Bridging
    removed dependency on Kitura-TestingFramework

--- Package.swift
+++ Package.swift
@@ -2,11 +2,7 @@ import PackageDescription
 
 let package = Package(
   name: "Mustache",
-  testDependencies: [
-    .Package(url: "git@github.com:IBM-Swift/Kitura-TestFramework.git", versions: Version(0,2,0)..<Version(0,3,0))
+  dependencies: [
+    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 2)
   ]
 )
-
-#if !os(Linux)
-    package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Bridging.git", versions: Version(0,2,0)..<Version(0,3,0)))
-#endif
