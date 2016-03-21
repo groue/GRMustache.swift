e930e87a90130b25542e6bbcdfad4b6809f54bd3
Vadim Eisenberg <vadime@il.ibm.com>
2016-03-21 22:56:15 +0200

    IBM-Swift/Kitura#170 rewrote processing JSONs in tests to use SwiftJSON

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
 import PackageDescription
 
+#if os(Linux)
+    let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
+    let swiftyJsonVersion = 3
+#else
+     let swiftyJsonUrl = "https://github.com/SwiftyJSON/SwiftyJSON.git"
+     let swiftyJsonVersion = 2
+#endif
+
 let package = Package(
   name: "Mustache",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 2)
+    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 2),
+    //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
+    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion)
   ]
 )
