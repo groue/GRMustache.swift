0d20c9cac7117128903adf6b693dae29cdd4ca38
Vadim Eisenberg <vadime@il.ibm.com>
2016-04-18 16:04:59 +0300

    IBM-Swift/Kitura#368 added _ argument label to Core Functions

--- Sources/CoreFunctions.swift
+++ Sources/CoreFunctions.swift
@@ -145,7 +145,7 @@ MustacheError of type RenderError.
 - parameter filter: A function `(MustacheBox) throws -> MustacheBox`.
 - returns: A FilterFunction.
 */
-public func Filter(filter: (MustacheBox) throws -> MustacheBox) -> FilterFunction {
+public func Filter(_ filter: (MustacheBox) throws -> MustacheBox) -> FilterFunction {
     return { (box: MustacheBox, partialApplication: Bool) in
         guard !partialApplication else {
             // This is a single-argument filter: we do not wait for another one.
@@ -179,7 +179,7 @@ MustacheError of type RenderError.
 - parameter filter: A function `(T?) throws -> MustacheBox`.
 - returns: A FilterFunction.
 */
-public func Filter<T>(filter: (T?) throws -> MustacheBox) -> FilterFunction {
+public func Filter<T>(_ filter: (T?) throws -> MustacheBox) -> FilterFunction {
     return { (box: MustacheBox, partialApplication: Bool) in
         guard !partialApplication else {
             // This is a single-argument filter: we do not wait for another one.
@@ -211,7 +211,7 @@ For example:
 - parameter filter: A function `([MustacheBox]) throws -> MustacheBox`.
 - returns: A FilterFunction.
 */
-public func VariadicFilter(filter: ([MustacheBox]) throws -> MustacheBox) -> FilterFunction {
+public func VariadicFilter(_ filter: ([MustacheBox]) throws -> MustacheBox) -> FilterFunction {
     
     // f(a,b,c) is implemented as f(a)(b)(c).
     //
@@ -265,7 +265,7 @@ allows you to chain pre-rendering filters without mangling HTML entities.
 - parameter filter: A function `(Rendering) throws -> Rendering`.
 - returns: A FilterFunction.
 */
-public func Filter(filter: (Rendering) throws -> Rendering) -> FilterFunction {
+public func Filter(_ filter: (Rendering) throws -> Rendering) -> FilterFunction {
     return { (box: MustacheBox, partialApplication: Bool) in
         guard !partialApplication else {
             // This is a single-argument filter: we do not wait for another one.
@@ -296,7 +296,7 @@ This example processes `T?` instead of `MustacheBox`, but the idea is the same.
 - parameter filter: A function `(MustacheBox, RenderingInfo) throws -> Rendering`.
 - returns: A FilterFunction.
 */
-public func Filter(filter: (MustacheBox, RenderingInfo) throws -> Rendering) -> FilterFunction {
+public func Filter(_ filter: (MustacheBox, RenderingInfo) throws -> Rendering) -> FilterFunction {
     return Filter { (box: MustacheBox) in
         // Box a RenderFunction
         return Box { (info: RenderingInfo) in
@@ -343,7 +343,7 @@ the `RenderingInfo` and `Rendering` types.
 - parameter filter: A function `(T?, RenderingInfo) throws -> Rendering`.
 - returns: A FilterFunction.
 */
-public func Filter<T>(filter: (T?, RenderingInfo) throws -> Rendering) -> FilterFunction {
+public func Filter<T>(_ filter: (T?, RenderingInfo) throws -> Rendering) -> FilterFunction {
     return Filter { (t: T?) in
         // Box a RenderFunction
         return Box { (info: RenderingInfo) in
@@ -500,7 +500,7 @@ bolded section has already been parsed with its template. You may prefer the raw
 - parameter lambda: A `String -> String` function.
 - returns: A RenderFunction.
 */
-public func Lambda(lambda: String -> String) -> RenderFunction {
+public func Lambda(_ lambda: String -> String) -> RenderFunction {
     return { (info: RenderingInfo) in
         switch info.tag.type {
         case .Variable:
@@ -617,7 +617,7 @@ using a Template instead of a lambda (see the documentation of
 - parameter lambda: A `() -> String` function.
 - returns: A RenderFunction.
 */
-public func Lambda(lambda: () -> String) -> RenderFunction {
+public func Lambda(_ lambda: () -> String) -> RenderFunction {
     return { (info: RenderingInfo) in
         switch info.tag.type {
         case .Variable:
