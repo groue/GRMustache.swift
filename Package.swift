d7b24e88f9b7cf779d59c87c6e2c6245a015996e
Ian Partridge <i.partridge@uk.ibm.com>
2016-04-29 13:50:26 +0100

    IBM-Swift/Kitura#418 Use 6.1 release of SwiftyJSON

--- Package.swift
+++ Package.swift
@@ -2,13 +2,14 @@ import PackageDescription
 
 let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
 let swiftyJsonVersion = 6
+let swiftyJsonMinorVersion = 1
 
 let package = Package(
   name: "Mustache",
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 2),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
-    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion)
+    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion, minor: swiftyJsonMinorVersion)
   ],
   exclude: ["Tests/Carthage"]
 )
