313c60069d06e3ae912698c6879d865fa1f8025e
Vadim Eisenberg <vadime@il.ibm.com>
2016-05-15 19:43:51 +0300

    IBM-Swift/Kitura#469 Range.start/endIndex changed to Range.lower/upperBound

--- Sources/TemplateCompiler.swift
+++ Sources/TemplateCompiler.swift
@@ -220,7 +220,7 @@ final class TemplateCompiler: TemplateTokenConsumer {
 //                            fatalError("Not implemented")
 //                        }
                         let templateString = token.templateString
-                        let innerContentRange = openingToken.range.endIndex..<token.range.startIndex
+                        let innerContentRange = openingToken.range.upperBound..<token.range.lowerBound
                         let sectionTag = TemplateASTNode.section(templateAST: templateAST, expression: closedExpression, inverted: false, openingToken: openingToken, innerTemplateString: templateString[innerContentRange])
 
                         compilationState.popCurrentScope()
@@ -250,7 +250,7 @@ final class TemplateCompiler: TemplateTokenConsumer {
 //                            fatalError("Not implemented")
 //                        }
                         let templateString = token.templateString
-                        let innerContentRange = openingToken.range.endIndex..<token.range.startIndex
+                        let innerContentRange = openingToken.range.upperBound..<token.range.lowerBound
                         let sectionTag = TemplateASTNode.section(templateAST: templateAST, expression: closedExpression, inverted: true, openingToken: openingToken, innerTemplateString: templateString[innerContentRange])
                         
                         compilationState.popCurrentScope()
