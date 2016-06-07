5eeec5893b878498f3339cb4ebf3148e746d461d
Vadim Eisenberg <vadime@il.ibm.com>
2016-06-07 22:59:24 +0300

    Revert "IBM-Swift/Kitura#504 increased the dependency version of SwiftyJSON"
    This reverts commit 92c9b2b455570098791f86cfb14db10026d59b62.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
 import PackageDescription
 
 let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
-let swiftyJsonVersion = 8
+let swiftyJsonVersion = 7
+let swiftyJsonMinorVersion = 0
 
 let package = Package(
   name: "Mustache",
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 2),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
-    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion)
+    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion, minor: swiftyJsonMinorVersion)
   ],
   exclude: ["Tests/Carthage", "Tests/vendor", "Tests/Info.plist"]
 )
