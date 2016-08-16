a6a05a74a545d1444ce7990b0b081f1ee7a7ba3e
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-16 13:55:49 +0300

    IBM-Swift/Kitura#675 removed extraneous function argument

--- Sources/URLEscapeHelper.swift
+++ Sources/URLEscapeHelper.swift
@@ -55,7 +55,7 @@ final class URLEscapeHelper : MustacheBoxable {
             // We don't know if the box contains a String, so let's escape its
             // rendering.
             return Box({ (info: RenderingInfo) -> Rendering in
-                let rendering = try box.render(info: info)
+                let rendering = try box.render(info)
                 return try self.filter(rendering: rendering)
 
             })
