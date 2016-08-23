d91b8aabecbc0ae8b6055f71dcb2938f0f5175d1
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-23 05:01:51 +0300

    IBM-Swift/Kitura#675 bumped SwiftyJSON dependency version
    removed SwiftyJSON related variables, use string and integer literals

--- Package.swift
+++ Package.swift
@@ -1,14 +1,11 @@
 import PackageDescription
 
-let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
-let swiftyJsonVersion = 12
-
 let package = Package(
   name: "Mustache",
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 27),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
-    .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion)
+      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 12, minor: 1)
   ],
   exclude: ["Tests/Carthage", "Tests/vendor", "Tests/Info.plist"]
 )
