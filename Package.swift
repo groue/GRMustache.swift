679724cf256680df09dcd42e9615716778bb4a6a
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-25 15:55:57 +0300

    IBM-Swift/Kitura#697 bumped dependency version of Bridging
    to 0.29.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Mustache",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 27),
+    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 0, minor: 29),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 12, minor: 1)
   ],
