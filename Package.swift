953714648c38788d5f57ea00f15e43c19931cbe0
Fumito Ito <fumito.ito@nex.nikkei.com>
2022-09-21 22:57:20 +0900

    show supported platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "Mustache",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9)
+    ],
     products: [
         .library(
             name: "Mustache",
